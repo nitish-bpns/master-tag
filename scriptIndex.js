3922bc19b191815ede496f8ed9b1502a0f90b94e
Nitish <nitish.bpns@gmail.com>
2022-07-11 14:45:54 +0530

    included cover images change  function and added map

---
+++ scriptIndex.js
@@ -0,0 +1,21 @@
+let nitish = document.querySelector('.img1');
+
+let main2 = 0
+
+function changeMain1() {
+
+    if (main2 === 0) {
+        nitish.src = './images/main2.png';
+        main2 = 1;
+    } else if (main2 === 1) {
+        nitish.src = './images/main3.png';
+        main2 = 2;
+    } else if (main2 === 2) {
+        nitish.src = './images/main1.png';
+        main2 = 0;
+    }
+
+};
+
+
+
